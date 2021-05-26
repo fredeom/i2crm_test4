7203062f5a1325fdec633645f5d82ba173e4013e
Alexander Smirnov <smirnov@microline.ru>
2021-05-26 20:28:41 +0300

    next002

--- frontend/web/index.php
+++ frontend/web/index.php
@@ -3,8 +3,6 @@
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'dev');
 
-echo "<h1>from frontend/web</h1>";
-exit(0);
 
 require __DIR__ . '/../../vendor/autoload.php';
 require __DIR__ . '/../../vendor/yiisoft/yii2/Yii.php';
@@ -16,6 +14,9 @@ $config = yii\helpers\ArrayHelper::merge(
     require __DIR__ . '/../../common/config/main-local.php',
     require __DIR__ . '/../config/main.php',
     require __DIR__ . '/../config/main-local.php'
-);
+  );
+
+echo "<h1>from frontend/web before Application run</h1>";
+exit(0);
 
 (new yii\web\Application($config))->run();
