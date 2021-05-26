870435452537c7ca609cce8716545e8209eeeb00
Alexander Smirnov <smirnov@microline.ru>
2021-05-26 23:41:51 +0300

    next008 flush main-local.php

--- common/config/main-local.php
+++ common/config/main-local.php
@@ -1,13 +1,4 @@
 <?php
 
 return [
-    'components' => [
-       'db' => [
-            'class' => 'yii\db\Connection',
-            'dsn' => 'pgsql:host=localhost;dbname=sms',
-            'username' => 'sms',
-            'password' => 'Rjvgjn123',
-            'charset' => 'utf8',
-        ],
-    ],
 ];
